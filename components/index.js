0e0ae844f90586334a61c5cf5ff87c58223514fb
Craig Thayer <cthayer@craigthayer.com>
2026-02-04 16:57:52 -0800

    add proper exports for package

---
+++ components/index.js
@@ -0,0 +1,7 @@
+
+import Base from './base';
+import InitIframe from './initIframe';
+import LoadEnv from './loadEnv';
+import Spinner from './spinner';
+
+export {Base, InitIframe, LoadEnv, Spinner};
---
+++ providers/index.js
@@ -0,0 +1,7 @@
+
+import Config from './config';
+import Context from './context';
+import Env from './env';
+import Session from './session';
+
+export {Config, Context, Env, Session};
